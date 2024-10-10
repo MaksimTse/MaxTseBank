40c602536ea8c18c9ac19d8ff45ad4eaa40b0a79
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2024-10-10 14:10:40 +0300

    Update register.svelte

--- src/routes/register.svelte
+++ src/routes/register.svelte
@@ -18,7 +18,7 @@
 
         if (response.user) {
             $session.user = response.user;
-            goto('/');
+            goto('/login');
         }
     }
 </script>
@@ -49,9 +49,9 @@
                     </fieldset>
                     <fieldset class="form-group">
                         <input class="form-control form-control-lg" type="password" required placeholder="Password" bind:value={password}>
-                        {#if password.length > 1 && password.length < 2}<sup><div class="alert alert-danger" role="alert">Password too short</div></sup>{/if}
+                        {#if password.length > 1 && password.length < 6}<sup><div class="alert alert-danger" role="alert">Password too short</div></sup>{/if}
                     </fieldset>
-                    <button class="btn btn-lg btn-primary pull-xs-right" disabled="{password.length <2}">
+                    <button class="btn btn-lg btn-primary pull-xs-right" disabled="{password.length <6}">
                         Sign up
                     </button>
                 </form>
